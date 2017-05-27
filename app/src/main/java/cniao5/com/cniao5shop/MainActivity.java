f6f23218598c06d965543c32946752c5d4da416b
zhanghaoqi97 <244993857@qq.com>
2017-05-27 17:25:51 +0800

    你好

--- app/src/main/java/cniao5/com/cniao5shop/MainActivity.java
+++ app/src/main/java/cniao5/com/cniao5shop/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //修改代码
         initTab();
 
     }
