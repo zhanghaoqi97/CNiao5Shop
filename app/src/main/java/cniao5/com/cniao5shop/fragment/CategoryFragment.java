b1374d81259a7442898251b34129bbb25364ec4a
zhanghaoqi97 <244993857@qq.com>
2017-05-27 14:59:32 +0800

    Initial commit

---
+++ app/src/main/java/cniao5/com/cniao5shop/fragment/CategoryFragment.java
@@ -0,0 +1,29 @@
+package cniao5.com.cniao5shop.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import java.util.ArrayList;
+import java.util.List;
+
+import cniao5.com.cniao5shop.R;
+
+/**
+ * Created by Ivan on 15/9/22.
+ */
+public class CategoryFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return  inflater.inflate(R.layout.fragment_category,container,false);
+    }
+}
+
+
+
---
+++ app/src/main/java/cniao5/com/cniao5shop/fragment/HotFragment.java
@@ -0,0 +1,29 @@
+package cniao5.com.cniao5shop.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import cniao5.com.cniao5shop.R;
+/**
+ * Created by Ivan on 15/9/22.
+ */
+public class HotFragment extends Fragment{
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view= inflater.inflate(R.layout.fragment_hot,container,false);
+
+        return view ;
+
+    }
+
+}
